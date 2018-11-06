e0138f8bdab4e03ff05d00c989d55a0f814f743f
Pascal Marleau-Proulx <pmarl012@uottawa.ca>
2018-11-06 18:11:55 -0500

    fix

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -16,12 +16,12 @@ public class ProductTest {
         assertEquals("Check the id of the product", "1", aProduct.getId());
     }
 
-    @Test
+    /*@Test
     public void checkProductPrice() {
         // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
         assertEquals("Check the price of the product", "180", aProduct.getPrice(0.1));
-    }
+    }*/
 
 
 }
